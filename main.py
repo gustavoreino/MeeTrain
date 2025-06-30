c3ac23b36dbcd04d70c9f32dead2aa88af36be2e
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2025-06-30 15:02:24 -0300

    Fix: executando ambos

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-"""
-Demonstration of the GazeTracking library.
-Check the README.md for complete documentation.
-"""
-
 import cv2
+import threading
 from gaze_tracking import GazeTracking
+from talk import iniciar_interface_talk  # Isso assume que talk.py está na mesma pasta
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+threading.Thread(target=iniciar_interface_talk, daemon=True).start()
 
 while True:
     try:
-
-        # We get a new frame from the webcam
         _, frame = webcam.read()
-
-        # We send this frame to GazeTracking to analyze it
         gaze.refresh(frame)
-
         frame = gaze.annotated_frame()
         text = ""
 
@@ -36,16 +29,19 @@ while True:
 
         left_pupil = gaze.pupil_left_coords()
         right_pupil = gaze.pupil_right_coords()
-        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-        cv2.imshow("Demo", frame)
+        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130),
+                    cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165),
+                    cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-        if cv2.waitKey(1) == 27:
+        cv2.imshow("Gaze Tracking", frame)
+
+        if cv2.waitKey(1) == 27:  # ESC
             break
     except KeyboardInterrupt:
         print("Exiting...")
         break
-   
+
 webcam.release()
 cv2.destroyAllWindows()
